2df623684571f8a833826948ad5244c145170dae
BI-TENG <shiebt.1026@gmail.com>
2022-02-18 17:37:23 +0800

    首頁切版

---
+++ js/style.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    // 主選單
+    var windowWidth = $(window).width();
+    var li = $('.main_navbar>li')
+    if (windowWidth > 1300) {
+        li.mouseenter(function() {
+            $(this).find($('.dropdown-menu')).toggleClass('active');
+            $(this).siblings(li).find('.dropdown-menu').removeClass('active');
+        });
+
+        li.mouseleave(function() {
+            $(this).find($('.dropdown-menu')).removeClass('active');
+        });
+
+        $('.main_navbar>li>a').focus(function() {
+            $(this).siblings($('.dropdown-menu')).toggleClass('active');
+            $(this).parent(li).siblings(li).find('.dropdown-menu').removeClass('active');
+        });
+
+        $(".dropdown-menu li:last-of-type a").focusout(function() {
+            $('.dropdown-menu').removeClass('active');
+        });
+
+    } else {
+        li.click(function() {
+            $(this).find($('.dropdown-menu')).slideToggle();
+            $(this).siblings(li).find('.dropdown-menu').slideUp();
+            $(this).find('a').toggleClass('active');
+            $(this).siblings(li).find('a').removeClass('active');
+        });
+    };
+
+    // 漢堡開關
+    // $('.navbar').hide();
+    $('.burger').click(function() {
+        $(this).toggleClass('open');
+        $('.navbar').toggleClass('show');
+    });
+
+
+})

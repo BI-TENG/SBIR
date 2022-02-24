616f190439939d32e511027a63760571e0bc5298
BI-TENG <shiebt.1026@gmail.com>
2022-02-24 17:34:05 +0800

    案件清單頁面

---
+++ js/enrol_page.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var windowWidth = $(window).width();
+    if (windowWidth < 991) {
+        $('.mobile_dropdown').hide();
+        $('.mobile_title').click(function() {
+            $('.mobile_dropdown').slideToggle();
+        });
+    };
+
+    $('.state').hide();
+    $('.desktop').click(function() {
+        $(this).next().slideToggle();
+        $(this).closest('.case_inner').siblings('.case_inner').children('.state').slideUp();
+    });
+})
